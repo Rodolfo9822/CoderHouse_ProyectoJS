4db92a75bedeb4f4117fc8fdd18ab0d3ee5e0f19
Rodolfo <orueta22@hotmail.com>
2022-06-28 10:39:32 -0500

    Se agrego el footer y se termino el diseño de index

--- js/eventos.js
+++ js/eventos.js
@@ -1,5 +1,8 @@
 
 const botontest = document.querySelector("#botontest");
+const instagram = document.querySelector("#instagram");
+const facebook = document.querySelector("#facebook");
+const twitter = document.querySelector("#twitter");
 
 botontest.addEventListener("click", ()=>{
     anunciosAutomaticos();
@@ -16,4 +19,14 @@ suscripcionBot.addEventListener("click", () => {
 }, false)
 
 
+instagram.addEventListener("mousemove", () =>{
+    instagram.title = "Instagram";
+});
 
+facebook.addEventListener("mousemove", () =>{
+    facebook.title = "Facebook";
+});
+
+twitter.addEventListener("mousemove", () =>{
+    twitter.title = "Twitter";
+});
